e48f69768690ad81041df2981650c4e9eeedcc26
Pablo Henrique SC <pabloph0077@gmail.com>
2023-10-27 11:05:57 -0300

    refactor: add new propertie

--- src/database/listEmails.js
+++ src/database/listEmails.js
@@ -1,1 +1,6 @@
-export const listEmails = [{ email: 'pablo@email.com', toReceive: true }]
+export const listEmails = [
+  { name: 'Pablo', email: 'pablo.h1@email.com', toReceive: true },
+  { name: 'Jefferson', email: 'jef.h2@email.com', toReceive: false },
+  { name: 'Gislene', email: 'gisa.h3@email.com', toReceive: false },
+  { name: 'Joyce', email: 'jo.h4@email.com', toReceive: true },
+]
