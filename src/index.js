9ba2a32015ad8dec85f58c09c98a209217887e7d
Pablo Henrique SC <pabloph0077@gmail.com>
2023-10-27 11:07:31 -0300

    feat: file main

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-import { currentDay } from "./currentDay.js";
+import { sendAllEmails } from './sendAllEmails.js'
+import { currentDay } from './subtask/currentDay.js'
 
-console.log(currentDay())
+function index() {
+  let currentDate = ''
+  const date = new Date('10/23/2023') // mm:dd:aaaa/
+
+  try {
+    currentDate = currentDay(date)
+  } catch (error) {
+    console.log('Data Inválida')
+    return false
+  }
+
+  if (currentDate === false) {
+    console.log('Data inválida')
+  } else if (currentDate === 1) {
+    sendAllEmails()
+  } else {
+    let diaSemana = [
+      'Domingo',
+      'Segunda-Feira',
+      'Terça-Feira',
+      'Quarta-Feira',
+      'Quinta-Feira',
+      'Sexta-Feira',
+      'Sábado',
+    ]
+    const dia = date.getDay()
+
+    console.log(
+      `De acordo com data atual, ${diaSemana[dia]}, não há email a serem enviados`
+    )
+  }
+}
+
+index()
